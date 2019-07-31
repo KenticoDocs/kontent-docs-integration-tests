bff5990972454acd86a750ec9c22603011fac030
Matus Backor <MatusB@kentico.com>
2019-07-31 12:25:14 +0200

    Fix tests after element codename change

--- shared/projectSettings.ts
+++ shared/projectSettings.ts
@@ -5,8 +5,8 @@ export const TEST_PROJECT_ID = process.env.TEST_PROJECT_ID || '';
 export const LIVE_API_KEY = process.env.LIVE_CM_API_KEY || '';
 export const LIVE_PROJECT_ID = process.env.LIVE_PROJECT_ID || '';
 
-export const CASCADE_PUBLISH_ID = process.env.KC_STEP_CASCADE_PUBLISH_ID || '';
-export const PUBLISHED_ID = process.env.KC_STEP_PUBLISHED_ID || '';
+export const CASCADE_PUBLISH_ID = process.env.CASCADE_PUBLISH_ID || '';
+export const PUBLISHED_ID = process.env.PUBLISHED_ID || '';
 export const PUBLISHER_TRIGGER_URL = process.env.PUBLISHER_TRIGGER_URL || '';
 
 export const SEARCH_APP_ID = process.env.SEARCH_APP_ID || '';
